014dec1fb89fd5bf7daadc5937a65a7022affe51
Gavin Martin <97780529+Siris4@users.noreply.github.com>
2024-06-07 23:20:27 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
